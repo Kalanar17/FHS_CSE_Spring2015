184b9221dba6e251f22e9c1d386fb880475e0a5f
Kalanar17 <mikep@fsusd.org>
2015-01-22 14:07:08 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+### This is a useless file
+### Only edit if you are Tristan or Zach
+
+def print_contributors():
+    ### Tristan, make the variable be equal to your name
+    
+    t = "Mr. Patterson"
+    
+    
+    ### Zach, make this variable be equal to your name
+    
+    z = "Mr. Patterson"
+    
+    print("The contributors are ",t," and ",z)
+    
